31e0f21326daa44ba112cf41f970cb38b5dc69be
fisart <1234fisart@gmail.com>
2015-10-23 17:15:05 +0200

    1

--- AF_Sonos/module.php
+++ AF_Sonos/module.php
@@ -68,9 +68,9 @@
 			SO_create_sonos_content_variable($parent_id);
 			SO_build_or_fix_sonos_variables($parent_id,"");
 			SO_build_or_fix_sonos_controls($parent_id,"");
-			SO_populate_variables($parent_id,"");
-			SO_create_profile($parent_id);
-			SO_build_or_fix_profile($parent_id,"");
+//			SO_populate_variables($parent_id,"");
+//			SO_create_profile($parent_id);
+//			SO_build_or_fix_profile($parent_id,"");
 
 //			SO_sonos_content( $parent_id);
 	   }
