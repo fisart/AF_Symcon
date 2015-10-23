0b6aebea6ab6e5691e3bcac64a5a2ebea8c73592
fisart <1234fisart@gmail.com>
2015-10-23 17:09:12 +0200

    1

--- AF_Sonos/module.php
+++ AF_Sonos/module.php
@@ -67,6 +67,7 @@
 			SO_read_sonos_data($parent_id);
 			SO_create_sonos_content_variable($parent_id);
 			SO_build_or_fix_sonos_variables($parent_id,"");
+			SO_build_or_fix_sonos_controls($parent_id,"");
 
 //			SO_sonos_content( $parent_id);
 	   }
@@ -76,7 +77,6 @@
 		{
 			global $action_ID, $parent_id, $ID_IP,$player_data_id,$Var_ID1,$Sonos_Data,$list,$script_id ;
 //			print_r($Sonos_Data);
-			SO_build_or_fix_sonos_controls($parent_id,"");
 			SO_populate_variables($parent_id,"");
 			SO_create_profile($parent_id);
 			SO_build_or_fix_profile($parent_id,"");
