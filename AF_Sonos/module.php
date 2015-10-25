a6600b7435b18fde4ec3d980b843c3dd34319ed2
fisart <1234fisart@gmail.com>
2015-10-25 08:42:41 +0100

    1

--- AF_Sonos/module.php
+++ AF_Sonos/module.php
@@ -656,7 +656,7 @@ public function 	create_profile() //Hier wird das Sonos Master Profil angelegt
 
 public function create_var($Name,$Root,$Type,$Profile,$switch)
 {
-  global $var_script_id;
+/*  global $var_script_id;
   $ID = @IPS_GetVariableIDByName ( $Name, $Root );
   if ($ID)
   {
@@ -666,12 +666,12 @@ public function create_var($Name,$Root,$Type,$Profile,$switch)
   		$ID = IPS_CreateVariable ( $Type );
   		IPS_SetName ( $ID,$Name );
   		IPS_SetParent ( $ID, $Root );
-//  		if ($switch) {IPS_SetVariableCustomAction ( $ID, $var_script_id );echo " ". $Profile." " ;}
+  		if ($switch) {IPS_SetVariableCustomAction ( $ID, $var_script_id );echo " ". $Profile." " ;}
   		IPS_SetVariableCustomProfile ( $ID, $Profile);
   }
 
   return $ID;
-
+*/
 }
 
 public function create_link($Parent,$Name,$Root,$ID)
