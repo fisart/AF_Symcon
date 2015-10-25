f5ff23a95fe4ed4fa6293e13caa0795208715573
fisart <1234fisart@gmail.com>
2015-10-25 16:55:56 +0100

    1

--- AF_Sonos/module.php
+++ AF_Sonos/module.php
@@ -24,8 +24,10 @@
 			$Sonos_Master_IP = $this->ReadPropertyString($master_ip_name_string); //Liest die Eigenschaft
 			$master_IP_id = $this->GetIDForIdent($master_ip_name_string);
 			SetValue($master_IP_id, $Sonos_Master_IP); //Beschreibt die Variable
-			$parent_id = IPS_GetObject($master_IP_id)['ParentID'];
-			
+			$parent_id  = IPS_CreateCategory();       // Kategorie anlegen
+			IPS_SetName($modul_id ,"AF SONOS MODUL"); // Kategorie benennen
+			IPS_SetParent($modul_id,0);
+
 			echo " PPPPP ".$parent_id."    ".$master_IP_id."  ";
 /*			SO_create_sonos_reader_socket($parent_id);
 			SO_create_sonos_text_parser($parent_id);
